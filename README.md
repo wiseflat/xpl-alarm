b5ad75fc4b6f62b135ce2819227ed4a5804dbdcd
Garcia <mathieu.garcia@enise.fr>
2015-03-23 09:15:08 +0100

    First import

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # xpl-alarm
 Node JS layer to execute mplayer through xPL
+
+## Objective
+
+Node JS layer to execute mplayer through xPL
+
+## Installation
+
+    $ git clone https://github.com/wiseflat/xpl-alarm.git
+    $ npm update
+
+## Usage
+
+You need to install the xPL_Hub first : https://github.com/Tieske/xPL4Linux
+
+Send xpl-cmnd to get the configuration of the module
+
+    $ xpl-send -m cmnd -c alarm.request
+
+Send xpl-cmnd to add/update a configuration
+
+    $ xpl-send -m cmnd -c alarm.config file=file1.mp3 enable=true
+
+Send xpl-cmnd to execute your script
+
+    $ xpl-send -m cmnd -c alarm.basic file=file1.mp3 command=play|stop|loop|timeLimit|stopTimeLimit
---
+++ index.js
@@ -0,0 +1,46 @@
+var xplalarm = require("./lib/xpl-alarm");
+
+var wt = new xplalarm(null, {
+	//xplSource: 'bnz-shell.wiseflat'
+        //xplLog: true
+});
+
+wt.init(function(error, xpl) { 
+
+	if (error) {
+		console.error(error);
+		return;
+	}
+        
+        // Load config file into hash
+        wt.readConfig();
+        wt.readBasic();
+        
+        // Send every minutes an xPL status message 
+        setInterval(function(){
+                wt.sendConfig();
+                wt.sendBasic();
+        }, 60 * 1000);
+        
+        //wt.play('http://www.tv-radio.com/station/france_inter_mp3/france_inter_mp3-128k.m3u');
+                
+        xpl.on("xpl:alarm.request", function(evt) {
+                if(evt.headerName == 'xpl-cmnd') wt.sendConfig();
+        });
+        
+        /*xpl.on("xpl:alarm.config", function(evt) {
+                if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.read();
+        }); */
+
+        xpl.on("xpl:alarm.basic", function(evt) {
+                if(evt.headerName == 'xpl-cmnd' && wt.validBasicSchema(evt.body)) {
+                    console.log(evt);
+                    if(evt.body.command == 'play') wt.play(evt.body);
+                    if(evt.body.command == 'stop') wt.stop(evt.body);
+                    if(evt.body.command == 'loop') wt.loop(evt.body);
+                    if(evt.body.command == 'timelimit') wt.timeLimit(evt.body);
+                    //if(evt.body.command == 'stopTimeLimit') wt.stopTimeLimit(evt.body);
+                }
+        });
+});
+
